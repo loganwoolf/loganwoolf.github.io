c2881e28beaf047942c85d28513d842744b9ce50
Logan Woolf <loganwoolf@gmail.com>
2022-03-29 14:20:08 -0600

    Create Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+const Navigation = (props) => {
+  return (
+    <nav>
+      <a href="#about-me">About Me</a>
+      <a href="#my-projects">My Projects</a>
+      <a href="#writings">Writings</a>
+      <a href="#resources">Resources</a>
+      <a href="#contact-me">Contact Me</a>
+    </nav>
+  );
+};
+
+export default Navigation;
