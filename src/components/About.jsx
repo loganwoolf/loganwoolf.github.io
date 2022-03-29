b3d8029d51ffe3b515925eb2d2bce2c2a1fbfb76
Logan Woolf <loganwoolf@gmail.com>
2022-03-29 14:27:00 -0600

    Create About component

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div id="about">
+      <h2 id="about-me">About Me</h2>
+      <p>I am a full stack web developer from Lethbridge, AB. 🇨🇦</p>
+      <p>I love to build things and am always learning.</p>
+    </div>
+  );
+};
+
+export default About;
