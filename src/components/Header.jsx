4e597f995737e821963d86f2598f104b87cffa9f
Logan Woolf <loganwoolf@gmail.com>
2022-03-29 14:19:27 -0600

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = (props) => {
+  return (
+    <header id="welcome">
+      <h1>Logan Woolf</h1>
+      <p>Full Stack Web Developer</p>
+    </header>
+  );
+};
+
+export default Header;
