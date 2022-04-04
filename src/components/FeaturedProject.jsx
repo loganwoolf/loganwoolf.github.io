e4eb318625d01a36aad82e810d42d37c1132f096
Logan Woolf <loganwoolf@gmail.com>
2022-04-04 17:20:35 -0600

    Lay out FeaturedProject

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -1,25 +1,56 @@
-import "./FeaturedProject.css"
+import "./FeaturedProject.css";
 
 const FeaturedProject = (props) => {
   return (
-    <main id="featured-project">
-      <img src="images/loyaltree/landing.png" alt="Loyaltree Landing Page" />
-      <div>
+    <article id="featured-project">
+      <img
+        className="feature-image"
+        src="images/loyaltree/landing.png"
+        alt="Loyaltree Landing Page"
+      />
+      <div className="feature-text">
         <h3>Loyaltree</h3>
         <p>
           A full stack web application that makes it easy for users to buy gift
           cards for local businesses and send them to friends and family.
         </p>
       </div>
-      <div>
+      <div className="feature-text">
         <p>
           Cards can be redeemed through the QR code scanner in the business
           owner's dashboard. Transaction history is available for both owners
           and card holders.
         </p>
       </div>
-      <img src="images/loyaltree/scan.png" alt="Loyaltree Landing Page" />
-    </main>
+      <img
+        className="feature-image"
+        src="images/loyaltree/scan.png"
+        alt="Loyaltree Landing Page"
+      />
+      <div className="feature-collage">
+        <img
+          className="feature-image-sm"
+          src="images/loyaltree/filter-clothing.png"
+          alt=""
+        />
+        <img
+          className="feature-image-sm"
+          src="images/loyaltree/filter-restaurant.png"
+          alt=""
+        />
+        <img
+          className="feature-image-sm"
+          src="images/loyaltree/filter-shop.png"
+          alt=""
+        />
+      </div>
+      <div className="feature-text">
+        <p>
+          Easily find businesses or gift cards using the filter element. Search
+          for store names or category tags. Results are displayed instantly.
+        </p>
+      </div>
+    </article>
   );
 };
 
