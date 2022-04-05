a01289e6d2803ae59c84930672fdf9a5e5702ec6
Logan Woolf <loganwoolf@gmail.com>
2022-04-05 17:33:58 -0600

    Style secondary project cards

--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -1,13 +1,17 @@
+import "./ProjectListItem.css"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const ProjectListItem = (props) => {
   const { title, sub, image, link, repo } = props;
   return (
-    <article>
+    <article className="project-list-item">
       <img src={image} alt="project preview" />
       <h3>{title}</h3>
       <p>{sub}</p>
       <footer>
-        <a href={link}>See it live</a>
-        <a href={repo}>See the code</a>
+        <a href={link}><FontAwesomeIcon icon="fa-solid fa-earth-americas" /> Live site</a>
+        <a href={repo}><FontAwesomeIcon icon="fa-brands fa-github" /> GitHub</a>
       </footer>
     </article>
   );
