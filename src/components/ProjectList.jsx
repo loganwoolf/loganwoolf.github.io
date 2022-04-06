5648f63b916e3349b15791dbe8cfd2e7493e648f
Logan Woolf <loganwoolf@gmail.com>
2022-04-06 07:10:14 -0600

    Modify feature project title text

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -30,7 +30,6 @@ const ProjectList = () => {
 
   return (
     <section id="projects">
-      <h2>Projects</h2>
       <FeaturedProject />
       <div className="project-list">{componentArr}</div>
     </section>
