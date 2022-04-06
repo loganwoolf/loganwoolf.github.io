ab5d90e956997c1df948e1048ce307d5e59d9491
Logan Woolf <loganwoolf@gmail.com>
2022-04-06 16:58:57 -0600

    Format SocialLinks component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,28 +2,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SocialLinks = () => {
   return (
-      <ul id="social-links">
-        <li>
-          <a href="http://github.com/loganwoolf">
-            <FontAwesomeIcon icon="fa-brands fa-github" />
-          </a>
-        </li>
-        <li>
-          <a href="http://codepen.io/loganwoolf">
-            <FontAwesomeIcon icon="fa-brands fa-codepen" />
-          </a>
-        </li>
-        <li>
-          <a href="http://github.com/loganwoolf">
-            <FontAwesomeIcon icon="fa-brands fa-twitter" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/logan-woolf-33607b100">
-            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-          </a>
-        </li>
-      </ul>
+    <ul id="social-links">
+      <li>
+        <a href="http://github.com/loganwoolf">
+          <FontAwesomeIcon icon="fa-brands fa-github" />
+        </a>
+      </li>
+      <li>
+        <a href="http://codepen.io/loganwoolf">
+          <FontAwesomeIcon icon="fa-brands fa-codepen" />
+        </a>
+      </li>
+      <li>
+        <a href="http://github.com/loganwoolf">
+          <FontAwesomeIcon icon="fa-brands fa-twitter" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/logan-woolf-33607b100">
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+        </a>
+      </li>
+    </ul>
   );
 };
 
